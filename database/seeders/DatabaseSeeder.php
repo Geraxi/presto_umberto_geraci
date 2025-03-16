ec40c983df12b27451a9ceceb3a91624ba9dcc05
Umberto Geraci <umbertogeraci@Umbertos-MBP-10.Home>
2025-03-17 00:37:38 +0100

    User story 1 completata

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,19 +5,36 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Category;
 
 class DatabaseSeeder extends Seeder
 {
     /**
      * Seed the application's database.
      */
-    public function run(): void
+   
+     public $categories=[
+        'elettronica',
+        'abbigliamento',
+        'salute e bellezza',
+        'casa e giardinaggio',
+        'giocattoli',
+        'sport',
+        'animali domestici',
+        'libri e riviste',
+        'accesori',
+        'motori'
+     ];
+   
+   
+     public function run(): void
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+       foreach ($this->categories as $category){
+        Category::create([
+            'name'=>$category
         ]);
+       }
     }
 }
